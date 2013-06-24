633f2dc0f96ec6e8103bcb855f19c4ecb6f4f124
Stephen Kuehn <stephenkuehn77@gmail.com>
2013-06-24 17:00:02 -0400

    Initial Empty template based on railstutorial.org/book rails 4.0.0.rc2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mootspace::Application.config.secret_key_base = '4a758119906c3b3d3895088a7d73be72b867c3eabf41003734c0fd4359f43bf72e054b163b8b0429306a5683f39836009c41048928ed13db938c2661199d92c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mootspace::Application.config.session_store :cookie_store, key: '_mootspace_session'
